994b1318682cf6cc40572947ae15eea9db89c605
Alexis Fricard <70525861+AlexisFricard@users.noreply.github.com>
2021-06-30 14:51:02 +0200

    Update settings.py

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'purbeurre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'webapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
